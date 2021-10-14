8e912a05aac418415b7ece9214265d77437adb85
Frank Wazeter <frank@wazeter.com>
2021-10-14 01:47:34 -0600

    rearranging structure of theme
    Making the theme more robust from the ground up to handle more component based architecture.

--- functions.php
+++ functions.php
@@ -31,6 +31,8 @@ if (! function_exists('mayuki_theme_setup' ) ) :
 	add_action( 'after_setup_theme', 'mayuki_theme_setup' );
 
 endif;
+/* load block-styles only when used - 07/01/2021, might not need */
+add_filter( 'should_load_separate_core_block_assets', '__return_true' );
 
 /**
  * Set the content width based on the theme's design and stylesheet.
@@ -56,17 +58,43 @@ function mayuki_content_width() {
 function mayuki_styles() {
 	wp_enqueue_style(
 		'mayuki-global',
-		get_theme_file_uri( 'assets/css/global.css' ),
+		get_theme_file_uri( 'assets/css/build/global.css' ),
 		'',
-		MAYUKI_VERSION,
-		'screen and (min-width: 601px)'
-	);
-	wp_enqueue_style(
-		'mayuki-mobile',
-		get_theme_file_uri( 'assets/css/mobile.css' ),
-		'',
-		MAYUKI_VERSION,
-		'screen and (max-width: 600px)'
+		MAYUKI_VERSION
 	);
 }
 add_action( 'wp_enqueue_scripts', 'mayuki_styles' );
+
+/**
+ * Attach extra styles to multiple blocks.
+ */
+function mayuki_enqueue_block_styles() {
+	// An array of blocks.
+	$styled_blocks = [ 'paragraph', 'code', 'cover', 'group' ];
+
+	foreach ( $styled_blocks as $block_name ) {
+		// Get the stylesheet handle. This is backwards-compatible and checks the
+		// availability of the `wp_should_load_separate_core_block_assets` function,
+		// and whether we want to load separate styles per-block or not.
+		$handle = (
+			function_exists( 'wp_should_load_separate_core_block_assets' ) &&
+			wp_should_load_separate_core_block_assets()
+		) ? "wp-block-$block_name" : 'wp-block-library';
+
+		// Get the styles.
+		$styles = file_get_contents( get_theme_file_path( "assets/css/build/$block_name.css" ) );
+
+		// Add frontend styles.
+		wp_add_inline_style( $handle, $styles );
+
+		// Add editor styles.
+		add_editor_style( "assets/css/build/$block_name.css" );
+		if ( file_exists( get_theme_file_path( "assets/css/build/$block_name.css" ) ) ) {
+			add_editor_style( "assets/css/build/$block_name.css" );
+		}
+	}
+}
+// Add frontend styles.
+add_action( 'wp_enqueue_scripts', 'mayuki_enqueue_block_styles' );
+// Add editor styles.
+add_action( 'admin_init', 'mayuki_enqueue_block_styles' );
